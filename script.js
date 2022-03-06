3bf8372d608e2e13f3c1d8865e3f3ea6bb04a885
yongLivelo <julioeduardolivelo@gmail.com>
2022-03-06 13:47:08 +0800

    add scrolling

--- script.js
+++ script.js
@@ -3,5 +3,9 @@
 const navLinks = document.querySelector(".nav__links");
 
 navLinks.addEventListener("click", (e) => {
-  console.log(e.target);
+  if (e.target.dataset.part) {
+    e.preventDefault();
+    const target = document.querySelector(`.${e.target.dataset.part}`);
+    target.scrollIntoView({ behavior: "smooth" });
+  }
 });
